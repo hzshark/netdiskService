239098faee3577cc31900435676cdef60857ca89
邵桦 <hua_shao@bestsign.cn>
2017-12-01 10:45:04 +0800

    add

---
+++ src/main/java/cn/bestsign/auto/netdiskService/NetdiskServiceApplication.java
@@ -0,0 +1,12 @@
+package cn.bestsign.auto.netdiskService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetdiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetdiskServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/bestsign/auto/netdiskService/NetdiskServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.bestsign.auto.netdiskService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetdiskServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
